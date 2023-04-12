91d6166616bc1f5ffb757009f71d6aafdd5d5a4b
MaiVnCh <mai.vn.ch@gmail.com>
2023-04-12 23:37:27 +0200

    add constant

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF7F5333);
+const kTextColor = Color(0x3E1B0E33);
+const kPhotoBackground = Color(0xB8B8B833);
+const kBackgroundColor = Color(0xE5E5E533);
+
+const double kDefaultPadding = 20;
+
