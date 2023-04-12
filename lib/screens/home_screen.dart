7f87ae31bd13496f6a9f4c665213953c32eaaff2
MaiVnCh <mai.vn.ch@gmail.com>
2023-04-12 23:39:25 +0200

    home_screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+    );
+  }
+}
